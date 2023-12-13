43503383872a1a3e6626c3c891866dea844ee1c7
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-12-14 00:31:33 +0530

    Displaying Offense Wise Classification

--- Pages/Boston.py
+++ Pages/Boston.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+colsForYearWiseDataFrame = ['OFFENSE_CODE' , 'OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'Location'];
+colsForOffenseWiseDataFrame = ['OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'Location'];
+
 def getData():
     # Files and Data/Boston Crime.csv
     path = 'Files and Data/Boston Crime.csv'
@@ -14,12 +17,18 @@ def getYears(crime_df):
     years.insert(0 , 'All Years')
     return years
 
+def getOffense(crime_df):
+    offenses = list(crime_df['OFFENSE_CODE_GROUP'].unique());
+    offenses.insert(0 , 'All Offenses')
+    return offenses
+
 def getYearWiseDataFrame(crime_df , yearOption):
-    returnCols = ['OFFENSE_CODE' , 'OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'Location'];
-    if yearOption == 'All Years': return crime_df[returnCols];
-    return crime_df[(crime_df['YEAR'] == yearOption)][returnCols];
+    if yearOption == 'All Years': return crime_df[colsForYearWiseDataFrame];
+    return crime_df[(crime_df['YEAR'] == yearOption)][colsForYearWiseDataFrame];
 
-
+def getOffenseWiseDataframe(crime_df , offenseOption):
+    if offenseOption == 'All Offenses': return crime_df[colsForOffenseWiseDataFrame]
+    return crime_df[(crime_df['OFFENSE_CODE_GROUP'] == offenseOption)][colsForOffenseWiseDataFrame]    
 
 def boston_page():
     st.title("Boston Page")
@@ -33,14 +42,27 @@ def boston_page():
     st.subheader("Year-Wise Classification of Crime")
     years = getYears(df);
     yearOption = st.selectbox(
-        f"Data related to the Crimes in Boston",
+        f"Year-Wise Data related to the Crimes in Boston",
         years,
         index=0,
-        placeholder="Select contact method...",
+        placeholder="Select An Year...",
     )
 
     st.dataframe(getYearWiseDataFrame(df , yearOption), use_container_width=True , hide_index=True)
     st.write('Showing Results for year: ', yearOption)
+
+    
+    st.subheader("Offense-Wise Classification of Crime")
+    offenses = getOffense(df);
+    offenseOption = st.selectbox(
+        f"Offense-Wise Data related to the Crimes in Boston",
+        offenses,
+        index=0,
+        placeholder="Select An Offense...",
+    )
+    
+    st.dataframe(getOffenseWiseDataframe(df , offenseOption), use_container_width=True , hide_index=True)
+    st.write('Showing Results for year: ', offenseOption)
 
 if __name__ == "__main__":
     boston_page()
