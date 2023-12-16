1dfaddd44d8f49f4e734d5d010ec4353a604aea0
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-12-17 01:22:02 +0530

    Chart and Data for All Offenses

--- Pages/Boston.py
+++ Pages/Boston.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import random
+import plotly.express as px
 
-colsForYearWiseDataFrame = ['OFFENSE_CODE' , 'OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'Lat' , 'Long'];
-colsForOffenseWiseDataFrame = ['OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'Lat' , 'Long'];
+colsForYearWiseDataFrame = ['OFFENSE_CODE' , 'OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'STREET' , 'Lat' , 'Long'];
+colsForOffenseWiseDataFrame = ['OFFENSE_DESCRIPTION' , 'OCCURRED_ON_DATE' , 'STREET' , 'Lat' , 'Long'];
 
 def getData():
     # Files and Data/Boston Crime.csv
@@ -31,12 +33,20 @@ def getOffenseWiseDataframe(crime_df , offenseOption):
     if offenseOption == 'All Offenses': return crime_df[colsForOffenseWiseDataFrame]
     return crime_df[(crime_df['OFFENSE_CODE_GROUP'] == offenseOption)][colsForOffenseWiseDataFrame]    
 
+def getColor():
+    red = random.randint(180, 255)
+    green = random.randint(180, 255)
+    blue = random.randint(180, 255)
+    hex_color = "#{:02X}{:02X}{:02X}".format(red, green, blue)
+    return hex_color
+
+
 def boston_page():
     st.title("Boston Page")
     st.write("Welcome to the Boston page!")
 
     df = getData();
-    st.map(df , latitude='Lat', longitude='Long')
+    st.map(df , latitude='Lat', longitude='Long' , color=getColor())
 
     st.title("Boston Crime Report");
 
@@ -45,7 +55,7 @@ def boston_page():
 
     values = st.slider(
         'Select the Year Range: ',
-        max(years) , min(years) , (years[1] , years[-1]))
+        max(years) , min(years) , (min(years) , max(years)))
     
     col1, col2 = st.columns(2)
 
@@ -55,9 +65,10 @@ def boston_page():
         st.dataframe(yearWiseData, use_container_width=True , hide_index=True)
 
     with col2:
-        st.map(yearWiseData , latitude='Lat', longitude='Long' , color='#4CB9E7')
+        st.map(yearWiseData , latitude='Lat', longitude='Long' , color=getColor())
     
     st.subheader("Offense-Wise Classification of Crime")
+
     offenses = getOffense(df);
     offenseOption = st.selectbox(
         f"Offense-Wise Data related to the Crimes in Boston",
@@ -66,8 +77,25 @@ def boston_page():
         placeholder="Select An Offense...",
     )
     
-    st.dataframe(getOffenseWiseDataframe(df , offenseOption), use_container_width=True , hide_index=True)
-    st.write('Showing Results for year: ', offenseOption)
+    offenseWiseData = getOffenseWiseDataframe(df , offenseOption);
+    st.write(f'{offenseWiseData.size} Records Found For' , offenseOption)
+    tab1, tab2 = st.tabs(['Chart' , 'Data']);
+    
+    with tab1:
+        if(offenseOption == 'All Offenses'):
+            allFrequencyDataFrame = df.groupby('OFFENSE_CODE_GROUP')['OFFENSE_CODE_GROUP'].count().reset_index(name='values');
+            # st.dataframe(allFrequencyDataFrame);
+            fig = px.pie(allFrequencyDataFrame, values='values' , names='OFFENSE_CODE_GROUP', title='Offense Classification')
+            # fig.show()
+            st.plotly_chart(fig, theme="streamlit")
+
+        else:
+            frequencyDataframe = df[df['OFFENSE_CODE_GROUP'] == offenseOption]['YEAR'].value_counts().sort_index();
+            st.bar_chart(frequencyDataframe , color=getColor() , use_container_width=True)
+
+    with tab2:
+        st.dataframe(offenseWiseData, use_container_width=True , hide_index=True)
+
 
 if __name__ == "__main__":
     boston_page()
